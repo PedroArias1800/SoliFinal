b4d4b6e3d95e63d2ff4b877ae7989b9d060f7ef4
jesusguevara26 <jesus.guevara@utp.ac.pa>
2021-07-19 08:03:51 -0500

    Pantalla Acerca De
    Añadí mi pantalla de "Acerca De" y los archivos que necesitaba para la misma.

---
+++ app/src/main/java/com/example/solifinal/AcercaDeActivity.java
@@ -0,0 +1,51 @@
+package com.example.solifinal;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AcercaDeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnInfo (View view)
+    {
+        new AlertDialog.Builder(this)
+                .setTitle("Información")
+                .setMessage("Info\nInfo\nInfo\nInfo\nInfo")
+                .setPositiveButton(android.R.string.ok,null)
+                .show();
+    }
+
+    public void btnEquipo (View view)
+    {
+        new AlertDialog.Builder(this)
+                .setTitle("Equipo de Desarrollo")
+                .setMessage("Info\nInfo\nInfo\nInfo\nInfo")
+                .setPositiveButton(android.R.string.ok,null)
+                .show();
+    }
+
+    public void btnYT(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/"));
+        startActivity(i);
+    }
+
+    public void btnFB(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/"));
+        startActivity(i);
+    }
+
+    public void btnIG(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/"));
+        startActivity(i);
+    }
+}
