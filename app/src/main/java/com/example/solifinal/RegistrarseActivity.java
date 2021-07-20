ac01cab98085428ef37c805f3d04407c0856beb5
Ramses1022 <86079720+Sike1022@users.noreply.github.com>
2021-07-19 21:13:22 -0500

    Pantalla Registrarse
    Añadi la  pantalla registrarse

--- app/src/main/java/com/example/solifinal/RegistrarseActivity.java
+++ app/src/main/java/com/example/solifinal/RegistrarseActivity.java
@@ -3,6 +3,7 @@ package com.example.solifinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistrarseActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class RegistrarseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
+    }
+
+
+    public void Registrar(View view){
+
+
     }
 }
