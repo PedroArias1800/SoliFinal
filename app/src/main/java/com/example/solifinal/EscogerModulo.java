b83cc0a2fc760e522cd63dc222762ee3d5694379
Ramses1022 <86079720+Sike1022@users.noreply.github.com>
2021-07-20 18:14:56 -0500

    Escoger Modulo pantalla

---
+++ app/src/main/java/com/example/solifinal/EscogerModulo.java
@@ -0,0 +1,14 @@
+package com.example.solifinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EscogerModulo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escoger_modulo);
+    }
+}
