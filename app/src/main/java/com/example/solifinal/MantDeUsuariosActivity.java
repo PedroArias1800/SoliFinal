1cd5eb8ffbdc5c46a220a5692a6d81a4e4e54830
Aurelio Morales <86581350+AMorales99@users.noreply.github.com>
2021-07-25 09:40:11 -0500

    ...
    ...

--- app/src/main/java/com/example/solifinal/MantDeUsuariosActivity.java
+++ app/src/main/java/com/example/solifinal/MantDeUsuariosActivity.java
@@ -4,18 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MantDeUsuariosActivity extends AppCompatActivity{
 
-    EditText nom;
-    String name;
+    RadioGroup tipo_usr;
+    ListView usrs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mant_usuarios);
+
+        IniControles();
+    }
+
+    private void IniControles(){
+        tipo_usr = (RadioGroup)findViewById(R.id.rgbTipoUsuario);
+        usrs = (ListView) findViewById(R.id.lstEditarUsuario);
     }
 
     public void CrearUsuario(View view) {
