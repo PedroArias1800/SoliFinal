6365e15d440d8f287053e447c40d43e6773102b0
PedroArias1800 <pedro.arias1@utp.ac.pa>
2021-07-25 22:29:33 -0500

    Quitando pantallas repetidas

--- app/src/main/java/com/example/solifinal/LoginActivity.java
+++ app/src/main/java/com/example/solifinal/LoginActivity.java
@@ -19,7 +19,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     EditText u, p;
     ProcesosDB _db;
